d4edb300b2282a54a50a47399bd2509bcd3ec7db
Shelly Li <shelly.li@moffitt.org>
2024-04-09 23:17:57 -0400

    Updated report

--- ESAS-DataManagement.R
+++ ESAS-DataManagement.R
@@ -33,10 +33,37 @@ ESAS %>%
   theme_minimal()
 
 ESAS %>%
-  filter(week <= )
+  filter(patient_id == 50) %>%
   mutate(patient_id = as.factor(`Patient ID`)) %>%
   ggplot(aes(x = week, y = `Total Score`, group = patient_id, color = patient_id)) +
   geom_line() +
   #  geom_point() +
   labs(title = "Spaghetti Plot of ESAS Data", x = "Week", y = "ESAS Total Score") +
   theme_classic()
+
+ESAS_n <- ESAS %>%
+  group_by(patient_id) %>%
+  mutate(n_weeks = n())
+
+model_1 <- lmer(`Total Score` ~ week + (1 + week| patient_id), data = ESAS, 
+                control = lmerControl(optimizer = "Nelder_Mead"))
+estimates_1 <- coef(model_1)$patient_id %>%
+  rownames_to_column(var = "patient_id") %>%
+  rename(intercept = `(Intercept)`, 
+         slope = week) %>%
+  merge(ESAS_n, by = "patient_id", all = T)
+write.csv(estimates_1, 
+          "/Volumes/jim_gonzalez_research/DATA/Jim/ESAS_datasets/MortonPlant/estimate_all.csv", row.names = T)
+
+model_2 <- lmer(`Total Score` ~ week + (1 + week| patient_id), data = ESAS_n[ESAS_n$n_weeks > 1,], 
+                control = lmerControl(optimizer = "Nelder_Mead")) 
+estimates_2 <- coef(model_2)$patient_id %>%
+  rownames_to_column(var = "patient_id") %>%
+  rename(intercept = `(Intercept)`, 
+         slope = week) %>%
+  merge(ESAS_n, by = "patient_id", all = T) %>%
+  mutate(intercept = ifelse(n_weeks == 1, `Total Score`, intercept), 
+         slope = ifelse(n_weeks == 1, 0, slope))
+  
+write.csv(estimates_2, 
+          "/Volumes/jim_gonzalez_research/DATA/Jim/ESAS_datasets/MortonPlant/estimate_MoreTime.csv", row.names = T)
